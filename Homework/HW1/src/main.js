706d41aa94368d820f3081a4f88f336d40cbade5
Joe <emailforinterwebs.jt@gmail.com>
2024-02-01 13:40:33 -0500

    hw1 done

--- Homework/HW1/src/main.js
+++ Homework/HW1/src/main.js
@@ -1,23 +1,52 @@
-"use strict";
+	import { randomElement } from "./utils.js";
 	
-	const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
-	
-	const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
-	
-	const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+	let words1;
+	let words2;
+	let words3;
 
-	generate();
+	//load arrays of babble in from babble-data.json
+	const loadBabble = () => {
+		const url = "data/babble-data.json";
+		const xhr = new XMLHttpRequest();
 
-	document.querySelector("#myButton").onclick = generate;
-
-	function randomElement(array)
-	{
-		return array[Math.floor(Math.random() * array.length)];
+		xhr.onload = (e) => babbleLoaded(e);
+		xhr.onerror = (e) => console.log(`In onerror - HTTP Status Code = ${e.target.status}`);
+		xhr.open("GET", url);
+		xhr.send();
 	}
 
-    function generate()
-    {
-		let str = `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
+	//generate given number of lines of babble
+	const generate = (num) => {
+		let html = "";
 
-		document.querySelector("#output").innerHTML = str;
-    }
+		for (let i = 0; i < num; i++) {
+			html += `<p>${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}</p>`;
+		}
+
+		document.querySelector("#output").innerHTML = html;
+	}
+
+	//parse babble data from json file
+	const babbleLoaded = (e) => {
+		let json;
+		console.log(`In onload - HTTP Status Code = ${e.target.status}`);
+
+		//check to make sure json file formatted correctly
+		try {
+			json = JSON.parse(e.target.responseText);
+		} catch {
+			document.querySelector("#output").innerHTML = "JSON file error";
+			return;
+		}
+
+		json = JSON.parse(e.target.responseText);
+		words1 = json["words1"];
+		words2 = json["words2"];
+		words3 = json["words3"];
+
+		document.querySelector("#btn-gen-1").onclick = () => generate(1);
+		document.querySelector("#btn-gen-5").onclick = () => generate(5);
+		generate(1);
+	}
+
+	loadBabble();
