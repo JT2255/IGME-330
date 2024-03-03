c445b7f72ced2f855a1dff9c1bb485eafdd3d2f9
Joe <emailforinterwebs.jt@gmail.com>
2024-03-03 12:38:23 -0500

    progress on HW2

---
+++ Homework/HW2/src/audio.js
@@ -0,0 +1,90 @@
+// 1 - our WebAudio context, **we will export and make this public at the bottom of the file**
+let audioCtx;
+
+// **These are "private" properties - these will NOT be visible outside of this module (i.e. file)**
+// 2 - WebAudio nodes that are part of our WebAudio audio routing graph
+let element, sourceNode, analyserNode, gainNode, biquadFilter, lowshelfBiquad, distortionFilter;
+
+// 3 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+    gain : .5,
+    numSamples : 256
+});
+
+// 4 - create a new array of 8-bit integers (0-255)
+// this is a typed array to hold the audio frequency data
+let audioData = new Uint8Array(DEFAULTS.numSamples / 2);
+
+// **Next are "public" methods - we are going to export all of these at the bottom of this file**
+function setupWebaudio(filePath) {
+    // 1 - The || is because WebAudio has not been standardized across browsers yet
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext();
+
+    // 2 - this creates an <audio> element
+    element = new Audio();
+
+    // 3 - have it point at a sound file
+    loadSoundFile(filePath);
+
+    // 4 - create an a source node that points at the <audio> element
+    sourceNode = audioCtx.createMediaElementSource(element);
+
+    // 5 - create an analyser node  
+    analyserNode = audioCtx.createAnalyser(); // note the UK spelling of "Analyser"
+    
+    /*
+    // 6
+    We will request DEFAULTS.numSamples number of samples or "bins" spaced equally 
+    across the sound spectrum.
+
+    If DEFAULTS.numSamples (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
+    the third is 344Hz, and so on. Each bin contains a number between 0-255 representing 
+    the amplitude of that frequency.
+    */ 
+
+    // fft stands for Fast Fourier Transform
+    analyserNode.fftSize = DEFAULTS.numSamples;
+
+    // 7 - create a gain (volume) node
+    gainNode = audioCtx.createGain();
+    gainNode.gain.value = DEFAULTS.gain;
+
+    biquadFilter = audioCtx.createBiquadFilter();
+    biquadFilter.type = "highshelf";
+    biquadFilter.frequency.setValueAtTime(1000, audioCtx.currentTime);
+    biquadFilter.gain.setValueAtTime(15, audioCtx.currentTime);
+
+    lowshelfBiquad = audioCtx.createBiquadFilter();
+    lowshelfBiquad.type = "lowshelf";
+    lowshelfBiquad.frequency.setValueAtTime(1000, audioCtx.currentTime);
+    lowshelfBiquad.gain.setValueAtTime(0, audioCtx.currentTime);
+
+    distortionFilter = audioCtx.createWaveShaper();
+
+    // 8 - connect the nodes - we now have an audio graph
+    sourceNode.connect(biquadFilter);
+    biquadFilter.connect(analyserNode);
+    analyserNode.connect(gainNode);
+    gainNode.connect(distortionFilter);
+    distortionFilter.connect(audioCtx.destination);
+}
+
+function loadSoundFile(filePath) {
+    element.src = filePath;
+}
+
+function playCurrentSound() {
+    element.play();
+}
+
+function pauseCurrentSound() {
+    element.pause();
+}
+
+function setVolume(value) {
+    value = Number(value); // make sure that it's a Number rather than a String
+    gainNode.gain.value = value;
+}
+
+export {audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode, biquadFilter, lowshelfBiquad, distortionFilter};
---
+++ Homework/HW2/src/main.js
@@ -0,0 +1,197 @@
+/*
+  main.js is primarily responsible for hooking up the UI to the rest of the application 
+  and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+
+import * as utils from './utils.js';
+import * as audio from './audio.js';
+import * as canvas from './canvas.js';
+
+const drawParams = {
+  showGradient : true,
+  showBars : true,
+  showCircles : true,
+};
+
+let highshelf;
+let lowshelf;
+let distortion;
+let distortionAmount = 20;
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+  sound1: "media/Red Shift.mp3"
+});
+
+function init() {
+  console.log("init called");
+  console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+  audio.setupWebaudio(DEFAULTS.sound1);
+  let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement, audio.analyserNode);
+  loop();
+}
+
+function setupUI(canvasElement) {
+  // A - hookup fullscreen button
+  const fsButton = document.querySelector("#fsButton");
+  const playButton = document.querySelector("#playButton");
+
+  // add .onclick event to button
+  fsButton.onclick = e => {
+    console.log("goFullscreen() called");
+    utils.goFullscreen(canvasElement);
+  };
+
+  // add .onclick event to button
+  playButton.onclick = e => {
+    console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+
+    // check is context is in suspended state (autoplay policy)
+    if (audio.audioCtx.state == "suspended") {
+      audio.audioCtx.resume();
+    }
+
+    console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+
+    if (e.target.dataset.playing == "no") {
+      // if track is currently paused, play it
+      audio.playCurrentSound();
+      e.target.dataset.playing = "yes";
+    }
+    else {
+      // if track is playing, pause it
+      audio.pauseCurrentSound();
+      e.target.dataset.playing = "no";
+    }
+  };
+
+  // C - hookup volume slider & label
+  let volumeSlider = document.querySelector("#volumeSlider");
+  let volumeLabel = document.querySelector("#volumeLabel");
+
+  // add .oninput event to slider
+  volumeSlider.oninput = e => {
+    // set the gain
+    audio.setVolume(e.target.value);
+
+    // update value of label to match value of slider
+    volumeLabel.innerHTML = Math.round((e.target.value / 2 * 100));
+  };
+
+  // set value of label to match initial value of slider
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  // D - hookup track <select>
+  let trackSelect = document.querySelector("#trackSelect");
+  // add .onchange event to <select>
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value);
+
+    // pause the current track if it is playing
+    if (playButton.dataset.playing == "yes") {
+      playButton.dispatchEvent(new MouseEvent("click"));
+    }
+  }
+
+  let gradientCheckbox = document.querySelector("#gradientCB");
+  let barCheckbox = document.querySelector("#barsCB");
+  let circleCheckbox = document.querySelector("#circlesCB");
+  let highshelfCheckbox = document.querySelector("#cb-highshelf");
+  let lowshelfCheckbox = document.querySelector("#cb-lowshelf");
+  let distortionCheckbox = document.querySelector("#cb-distortion");
+  let distortionSlider = document.querySelector("#slider-distortion");
+  highshelf = highshelfCheckbox.checked;
+  lowshelf = lowshelfCheckbox.checked;
+  distortion = distortionCheckbox.checked;
+  distortionAmount = Number(distortionSlider.value);
+
+  gradientCheckbox.onchange = e => {
+    if (e.target.checked) { drawParams.showGradient = true; }
+    else { drawParams.showGradient = false; }
+  }
+
+  barCheckbox.onchange = e => {
+    if (e.target.checked) { drawParams.showBars = true; }
+    else { drawParams.showBars = false; } 
+  }
+
+  circleCheckbox.onchange = e => {
+    if (e.target.checked) { drawParams.showCircles = true; }
+    else { drawParams.showCircles = false; } 
+  }
+
+  highshelfCheckbox.onchange = e => { 
+    highshelf = e.target.checked;
+    toggleHighshelf();
+  }
+
+  lowshelfCheckbox.onchange = e => {
+    lowshelf = e.target.checked;
+    toggleLowshelf();
+  }
+
+  distortionCheckbox.onchange = e => {
+    distortion = e.target.checked;
+    toggleDistortion();
+  }
+
+  distortionSlider.onchange = e => {
+    distortionAmount = Number(e.target.value);
+    toggleDistortion();
+  }
+
+} // end setupUI
+
+function loop() {
+  setTimeout(loop, 1000 / 60);
+  canvas.draw(drawParams);
+}
+
+function toggleHighshelf() {
+  if (highshelf) {
+    audio.biquadFilter.frequency.setValueAtTime(1000, audio.audioCtx.currentTime);
+    audio.biquadFilter.gain.setValueAtTime(15, audio.audioCtx.currentTime);
+  }
+  else {
+    audio.biquadFilter.gain.setValueAtTime(0, audio.audioCtx.currentTime);
+  }
+}
+
+function toggleLowshelf() {
+  if (lowshelf) {
+    audio.lowshelfBiquad.frequency.setValueAtTime(1000, audio.audioCtx.currentTime);
+    audio.lowshelfBiquad.gain.setValueAtTime(15, audio.audioCtx.currentTime);
+  }
+  else {
+    audio.lowshelfBiquad.gain.setValueAtTime(0, audio.audioCtx.currentTime);
+  }
+}
+
+function toggleDistortion() {
+  if (distortion) {
+    audio.distortionFilter.curve = null;
+    audio.distortionFilter.curve = makeDistortionCurve(distortionAmount);
+  }
+  else {
+    audio.distortionFilter.curve = null;
+  }
+}
+
+function makeDistortionCurve(amount = 20) {
+  let n_samples = 256, curve = new Float32Array(n_samples);
+
+  for (let i = 0; i < n_samples; ++i) {
+    let x = i * 2 / n_samples - 1;
+    curve[i] = x * Math.sin(x) * amount/5;
+  }
+
+  return curve;
+}
+
+export { init };
